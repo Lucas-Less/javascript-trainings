6ead5b09f0eef5429447ac50d561f5842729b7d5
lucasCgod <lucas.silva2k@hotmail.com>
2022-03-14 20:39:16 -0300

    exemplos da aula

---
+++ reckon.js
@@ -0,0 +1,13 @@
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const operacaoMatematica = primeiroNumero + segundoNumero;
+const numeroFloat = 3.44;
+const floatSem0 = .66;
+const operacao2 = numeroFloat + floatSem0;
+const string = "vrau";
+
+console.log(string * segundoNumero); // NaN
+
+
+
---
+++ stringMani.js
@@ -0,0 +1,6 @@
+const text1 = "Olá fdp";
+const text2 = 'va pu caralho';
+const strNumbers = "778954";
+
+const citacao = 'o leo disse "congadaiola"';
+console.log(citacao + " " + text2)
