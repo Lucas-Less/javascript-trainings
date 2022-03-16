ebdd2e5c7171db05566677071bfbfb335b805518
lucasCgod <lucas.silva2k@hotmail.com>
2022-03-15 21:31:24 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,6 @@
+const numero = 3;
+const numeroString = '3';
+
+console.log(numero == numeroString);
+
+console.log(numero + Number(numeroString));
---
+++ script.js
@@ -0,0 +1,35 @@
+/*var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+console.log(area)
+var area;
+
+let forma = 'retangulo'
+let altura = 7;
+let comprimento = 10;
+let area;
+
+if(forma === 'retangulo'){
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+
+console.log(area) */
+
+const forma = 'triangulo';
+const altura = 5;
+const compriment0o = 7;
+let area; 
+
+if(forma === 'quadrado'){
+
+    area = altura * comprimento;
+
+}else{
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
