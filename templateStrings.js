9fd9c55cb1310c0270a5e9f801879a03689299d5
lucasCgod <lucas.silva2k@hotmail.com>
2022-04-05 10:42:22 -0300

    commit deu certo

---
+++ templateStrings.js
@@ -0,0 +1,9 @@
+const nome = "lucas";
+const idade = 2021 ;
+const cidadeNascimento = "sp";
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeNascimento; 
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+
+
+console.log(apresentacao)
